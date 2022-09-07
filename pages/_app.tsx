949becf17b875eaa0c33cc9cd42502566f8ffa88
youkou <youkousai@gmail.com>
2022-09-07 22:51:39 +0900

    initial demo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,36 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { SessionProvider, signIn, useSession } from 'next-auth/react';
+import * as React from 'react';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+type AuthProps = {
+  children: React.ReactNode;
+};
+
+function Auth({ children }: AuthProps): JSX.Element {
+  const { data: session, status } = useSession();
+
+  const loading = status === 'loading';
+  const isLogin = !!session?.user;
+
+  React.useEffect(() => {
+    if (loading) return;
+    if (!isLogin) signIn();
+  }, [isLogin, loading]);
+
+  if (isLogin) {
+    return <>{children}</>;
+  }
+
+  return <>loading</>;
 }
 
-export default MyApp
+export default function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <Auth>
+        <Component {...pageProps} />
+      </Auth>
+    </SessionProvider>
+  );
+}
